fe215bbad1ef3c884ea8699262d27b688223400e
Robb Surridge <robb.surridge@autodesk.com>
2017-05-19 14:20:12 -0400

    Automatic version history generation for C APIs.

---
+++ source/editor_c/versions/Release_1_9.js
@@ -0,0 +1,83 @@
+var version_diff_editor_c_Release_1_9 = {
+  "class":[],
+  "func":[],
+  "example":[],
+  "enumvalue":[],
+  "typedef":[],
+  "file":[],
+  "define":[],
+  "union":[{
+      "change":"removed",
+      "name":"union_config_primitive_value",
+      "version":"Release 1.9",
+      "display_name":"ConfigPrimitiveValue"
+    }],
+  "variable":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_config_value_struct_1ac1e257f4ff6a38c80e41418c4eee2f4e\" data-reftooltip-refid=\"editor_c\"><a class=\"el\" href=\"editor__plugin__api_8h.html#line_99\">ConfigValueStruct::_array</a></span>",
+      "version":"Release 1.9",
+      "name":"struct_config_value_struct_1ac1e257f4ff6a38c80e41418c4eee2f4e"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_config_value_struct_1a395dbb72b504daecb3df088c9a3583cf\" data-reftooltip-refid=\"editor_c\"><a class=\"el\" href=\"editor__plugin__api_8h.html#line_98\">ConfigValueStruct::_object</a></span>",
+      "version":"Release 1.9",
+      "name":"struct_config_value_struct_1a395dbb72b504daecb3df088c9a3583cf"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_config_value_struct_1a8713592a80b1350315b243c3f6c15362\" data-reftooltip-refid=\"editor_c\"><a class=\"el\" href=\"editor__plugin__api_8h.html#line_100\">ConfigValueStruct::_data</a></span>",
+      "version":"Release 1.9",
+      "name":"struct_config_value_struct_1a8713592a80b1350315b243c3f6c15362"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_config_value_struct_1ad343959e47b9559387dcb2d1f409d2a6\" data-reftooltip-refid=\"editor_c\"><a class=\"el\" href=\"editor__plugin__api_8h.html#line_97\">ConfigValueStruct::_allocator</a></span>",
+      "version":"Release 1.9",
+      "name":"struct_config_value_struct_1ad343959e47b9559387dcb2d1f409d2a6"
+    },{
+      "change":"removed",
+      "name":"union_config_primitive_value_1aa17ead9a486aa3ab9cddfd40997875f9",
+      "version":"Release 1.9",
+      "display_name":"str"
+    },{
+      "change":"removed",
+      "name":"union_config_primitive_value_1a56ab45fba27a01191d322d7a8c7459a2",
+      "version":"Release 1.9",
+      "display_name":"obj"
+    },{
+      "change":"removed",
+      "name":"union_config_primitive_value_1a5873ea22a732d8d0b4b99c1a6b13762b",
+      "version":"Release 1.9",
+      "display_name":"handle"
+    },{
+      "change":"removed",
+      "name":"union_config_primitive_value_1adb24f1aa1c77d763b225ec9799130673",
+      "version":"Release 1.9",
+      "display_name":"arr"
+    },{
+      "change":"removed",
+      "name":"struct_config_value_struct_1a2ea4657848fb8e2f793d5ccacc036dd9",
+      "version":"Release 1.9",
+      "display_name":"ConfigValueStruct::reserved"
+    },{
+      "change":"removed",
+      "name":"union_config_primitive_value_1a7f82b76b8c58733be7c1e653f4c41e8e",
+      "version":"Release 1.9",
+      "display_name":"number"
+    },{
+      "change":"removed",
+      "name":"struct_config_data_api_1a3601c701ebf6c4b7558e1d383056efc8",
+      "version":"Release 1.9",
+      "display_name":"ConfigDataApi::from_type"
+    },{
+      "change":"removed",
+      "name":"union_config_primitive_value_1a1d187e6f217a371018b3851572b25429",
+      "version":"Release 1.9",
+      "display_name":"boolean"
+    },{
+      "change":"removed",
+      "name":"union_config_primitive_value_1a07875dd50baba5880638658b343fa4f9",
+      "version":"Release 1.9",
+      "display_name":"int32"
+    }],
+  "struct":[],
+  "enum":[]
+};
