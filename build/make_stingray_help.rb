cd3b14336ca1839a7b1aad77437e9c71a9a9b060
Robb Surridge <robb.surridge@autodesk.com>
2017-09-13 09:27:37 -0400

    Change the component name for the main SR help, to avoid conflicting with the 1.9 material.

--- build/make_stingray_help.rb
+++ build/make_stingray_help.rb
@@ -1,5 +1,5 @@
 
-puts("Starting to build the Stingray component help...")
+puts("Starting to build the Stingray interactive component help...")
 
 $script_dir = File.expand_path(File.dirname(__FILE__))
 Dir.chdir("#{$script_dir}")
@@ -9,11 +9,11 @@ require_relative 'scripts/set_upload_variables'
 
 # Set environment variables used to generate the doc and publish it on AKN.
 
-ENV["SR_DOC_AKN_COMPONENT_ID"] = "Stingray-Help"
+ENV["SR_DOC_AKN_COMPONENT_ID"] = "Interactive-Help"
 ENV["SR_DOC_AKN_PRODUCT"] = "Stingray"
 ENV["SR_DOC_AKN_RELEASE"] = "NA"
-ENV["SR_DOC_PROJECT_TITLE"] = "Stingray Help"
-ENV["SR_DOC_LOCAL_OUTPUT"] = "stingray_help"
+ENV["SR_DOC_PROJECT_TITLE"] = "Stingray Interactive Help"
+ENV["SR_DOC_LOCAL_OUTPUT"] = "interactive_help"
 
 ENV["SR_DOC_TITLE_FORMAT"] = "$TITLE$"
 
