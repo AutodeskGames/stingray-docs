205b308a13a103fe63f063a5edcb321be0b5618f
Robb Surridge <robb.surridge@autodesk.com>
2017-11-16 15:25:42 -0500

    Add 2.0 version histories for all API references.

---
+++ source/engine_c/versions/Release_2_0.js
@@ -0,0 +1,156 @@
+var version_diff_engine_c_Release_2_0 = {
+  "class":[],
+  "func":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"namespacestingray__plugin__foundation_1a3a68510c869218d76770d4_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html#line_53\">stingray_plugin_foundation::float_to_half</a></span>",
+      "version":"Release 2.0",
+      "name":"namespacestingray__plugin__foundation_1a3a68510c869218d76770d4256ba2c20e"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"namespacestingray__plugin__foundation_1_1half__globals_1ad60d5_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html#line_45\">stingray_plugin_foundation::half_globals::init</a></span>",
+      "version":"Release 2.0",
+      "name":"namespacestingray__plugin__foundation_1_1half__globals_1ad60d5ca4bf75a2c882052e1e94dc2e6b"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"namespacestingray__plugin__foundation_1_1half__globals_1aeeadd_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html#line_50\">stingray_plugin_foundation::half_globals::special_case_convert</a></span>",
+      "version":"Release 2.0",
+      "name":"namespacestingray__plugin__foundation_1_1half__globals_1aeeadd26a8faf7593658d4bac54377b08"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"namespacestingray__plugin__foundation_1adcc844fba56fb8d1d5d2fa_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html#line_68\">stingray_plugin_foundation::half_to_float</a></span>",
+      "version":"Release 2.0",
+      "name":"namespacestingray__plugin__foundation_1adcc844fba56fb8d1d5d2fa55d9dcc293"
+    }],
+  "example":[],
+  "enumvalue":[],
+  "typedef":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"plugin__api_8h_1aac95c7aeb9605b4e496af66127467a5c_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_650\">SceneDatabaseCompileFunction</a></span>",
+      "version":"Release 2.0",
+      "name":"plugin__api_8h_1aac95c7aeb9605b4e496af66127467a5c"
+    }],
+  "file":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"half_8h\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html\">half.h</a></span>",
+      "version":"Release 2.0",
+      "name":"half_8h"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"half_8inl\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8inl.html\">half.inl</a></span>",
+      "version":"Release 2.0",
+      "name":"half_8inl"
+    }],
+  "define":[],
+  "union":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"unionstingray__plugin__foundation_1_1half__globals_1_1uif_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html#line_46\">stingray_plugin_foundation::half_globals::uif</a></span>",
+      "version":"Release 2.0",
+      "name":"unionstingray__plugin__foundation_1_1half__globals_1_1uif"
+    }],
+  "variable":[{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_lua_api_1a578ab6dc01c1ab7beb0bbbb56ae2b5e1_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_585\">LuaApi::reserved</a></span>",
+      "details":"<div><div class='code version-removed-inner' style='display:inline-block'>void* LuaApi::reserved[32]</div></div><div><div class='code version-added-inner' style='display:inline-block'>void* LuaApi::reserved[28]</div></div>",
+      "version":"Release 2.0",
+      "name":"struct_lua_api_1a578ab6dc01c1ab7beb0bbbb56ae2b5e1"
+    },{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"unionstingray__plugin__foundation_1_1half__globals_1_1uif_1a83_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html#line_48\">f</a></span>",
+      "details":"<div><div class='code' style='display:inline-block'>float stingray_plugin_foundation::const_config::Value::f</div></div><div><div class='code version-added-inner' style='display:inline-block'>float stingray_plugin_foundation::half_globals::uif::f</div></div>",
+      "version":"Release 2.0",
+      "name":"unionstingray__plugin__foundation_1_1half__globals_1_1uif_1a83838d2e12bce8b9a50d838413e2c936"
+    },{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"unionstingray__plugin__foundation_1_1half__globals_1_1uif_1abf_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html#line_47\">i</a></span>",
+      "details":"<div><div class='code' style='display:inline-block'>int stingray_plugin_foundation::const_config::Value::i</div></div><div><div class='code version-added-inner' style='display:inline-block'>unsigned int stingray_plugin_foundation::half_globals::uif::i</div></div>",
+      "version":"Release 2.0",
+      "name":"unionstingray__plugin__foundation_1_1half__globals_1_1uif_1abffb3d27bc78e1f57b54f8263d8b64a5"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_application_api_1a6b63c7e930de478f58e87521082874e2_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1643\">ApplicationApi::swap_chain_handle_for_window</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_application_api_1a6b63c7e930de478f58e87521082874e2"
+    },{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_application_options_api_1a242eea3db08d9c5f917c5e9353453_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1668\">ApplicationOptionsApi::reserved</a></span>",
+      "details":"<div><div class='code version-removed-inner' style='display:inline-block'>void* ApplicationOptionsApi::reserved[32]</div></div><div><div class='code version-added-inner' style='display:inline-block'>void* ApplicationOptionsApi::reserved[31]</div></div>",
+      "version":"Release 2.0",
+      "name":"struct_application_options_api_1a242eea3db08d9c5f917c5e9353453b0b"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_u___bounding_volume_1afabd8e2bcbef5485b884dc9fc4f06f72_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1279\">U_BoundingVolume::radius</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_u___bounding_volume_1afabd8e2bcbef5485b884dc9fc4f06f72"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_u___bounding_volume_1a0f116bb606e566be1c5bc7022b4d666a_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1277\">U_BoundingVolume::min</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_u___bounding_volume_1a0f116bb606e566be1c5bc7022b4d666a"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"namespacestingray__plugin__foundation_1_1half__globals_1a3f50c_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"half_8h.html#line_44\">stingray_plugin_foundation::half_globals::_half_lookup</a></span>",
+      "version":"Release 2.0",
+      "name":"namespacestingray__plugin__foundation_1_1half__globals_1a3f50cc48c1493fb19733d8aca6c8782c"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_unit_c_api_1aec438784df6e67a26fda7ea6a1263691_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"c__api__unit_8h.html#line_90\">UnitCApi::has_animation_blender</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_unit_c_api_1aec438784df6e67a26fda7ea6a1263691"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_unit_api_1a63f8686afdaab4e1de8c0566c0a4bc04_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1437\">UnitApi::mesh_bounding_volume</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_unit_api_1a63f8686afdaab4e1de8c0566c0a4bc04"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_data_compiler_api_1aee540447f80514c53512bd7e88e5641d_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_732\">DataCompilerApi::add_scene_database_compiler</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_data_compiler_api_1aee540447f80514c53512bd7e88e5641d"
+    },{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_data_compiler_api_1a939c515cab38c016e6f8ebd660139868_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_735\">DataCompilerApi::reserved</a></span>",
+      "details":"<div><div class='code version-removed-inner' style='display:inline-block'>void* DataCompilerApi::reserved[32]</div></div><div><div class='code version-added-inner' style='display:inline-block'>void* DataCompilerApi::reserved[31]</div></div>",
+      "version":"Release 2.0",
+      "name":"struct_data_compiler_api_1a939c515cab38c016e6f8ebd660139868"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_application_options_api_1a7986d8b730254eca6c99bc8b2ba9e_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1665\">ApplicationOptionsApi::is_rendering_enabled</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_application_options_api_1a7986d8b730254eca6c99bc8b2ba9e53a"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_u___bounding_volume_1a133ef4b6429be1f00e73ba8d1fdfcece_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1278\">U_BoundingVolume::max</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_u___bounding_volume_1a133ef4b6429be1f00e73ba8d1fdfcece"
+    },{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_unit_api_1aaf0582821ee5b3641ca940c8ab6c679e_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1440\">UnitApi::reserved</a></span>",
+      "details":"<div><div class='code version-removed-inner' style='display:inline-block'>void* UnitApi::reserved[32]</div></div><div><div class='code version-added-inner' style='display:inline-block'>void* UnitApi::reserved[30]</div></div>",
+      "version":"Release 2.0",
+      "name":"struct_unit_api_1aaf0582821ee5b3641ca940c8ab6c679e"
+    },{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_file_system_api_1a5580d55bda5da3c269b038d74681f317_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1755\">FileSystemApi::reserved</a></span>",
+      "details":"<div><div class='code version-removed-inner' style='display:inline-block'>void* FileSystemApi::reserved[32]</div></div><div><div class='code version-added-inner' style='display:inline-block'>void* FileSystemApi::reserved[31]</div></div>",
+      "version":"Release 2.0",
+      "name":"struct_file_system_api_1a5580d55bda5da3c269b038d74681f317"
+    },{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_application_api_1ae36d1b43d56850354e466629eadd3bc2_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1646\">ApplicationApi::reserved</a></span>",
+      "details":"<div><div class='code version-removed-inner' style='display:inline-block'>void* ApplicationApi::reserved[32]</div></div><div><div class='code version-added-inner' style='display:inline-block'>void* ApplicationApi::reserved[31]</div></div>",
+      "version":"Release 2.0",
+      "name":"struct_application_api_1ae36d1b43d56850354e466629eadd3bc2"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"structstingray__plugin__foundation_1_1_scene_import_options_1a_22\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"scene__tree_8h.html#line_421\">stingray_plugin_foundation::SceneImportOptions::bake_scale</a></span>",
+      "version":"Release 2.0",
+      "name":"structstingray__plugin__foundation_1_1_scene_import_options_1a36408428c56a802f911ba1978a04d532"
+    }],
+  "struct":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_u___bounding_volume_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_1275\">U_BoundingVolume</a></span>",
+      "version":"Release 2.0",
+      "name":"struct_u___bounding_volume"
+    }],
+  "enum":[]
+};
