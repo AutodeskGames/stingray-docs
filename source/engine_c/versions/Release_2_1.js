7df473aae24776ba1ef487d5fa4e9e6fcc84c605
Robb Surridge <robb.surridge@autodesk.com>
2018-02-06 15:01:56 -0500

    Add version history for editor and engine C APIs

---
+++ source/engine_c/versions/Release_2_1.js
@@ -0,0 +1,44 @@
+var version_diff_engine_c_Release_2_1 = {
+  "class":[],
+  "func":[],
+  "example":[],
+  "enumvalue":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"plugin__api_8h_1adaa4ab4336503603b91000ca436f5199ac0e56c1d78a5_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_228\">RC_CASCADED_SHADOW_MAPPING</a></span>",
+      "version":"Release 2.1",
+      "name":"plugin__api_8h_1adaa4ab4336503603b91000ca436f5199ac0e56c1d78a5b4e9544aabfbe4883e2a"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"plugin__api_8h_1adaa4ab4336503603b91000ca436f5199ad504f66c821b_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_229\">RC_ATLASED_SHADOW_MAPPING</a></span>",
+      "version":"Release 2.1",
+      "name":"plugin__api_8h_1adaa4ab4336503603b91000ca436f5199ad504f66c821bfbbebd99eab068ef3559"
+    }],
+  "typedef":[],
+  "file":[],
+  "define":[],
+  "union":[],
+  "variable":[{
+      "change":"modified",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_render_callbacks_plugin_api_1adbf53ddaa36b8c3319d4d086f_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_265\">RenderCallbacksPluginApi::reserved</a></span>",
+      "details":"<div><div class='code version-removed-inner' style='display:inline-block'>void* RenderCallbacksPluginApi::reserved[32]</div></div><div><div class='code version-added-inner' style='display:inline-block'>void* RenderCallbacksPluginApi::reserved[30]</div></div>",
+      "version":"Release 2.1",
+      "name":"struct_render_callbacks_plugin_api_1adbf53ddaa36b8c3319d4d086fbe102e8"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_render_callbacks_plugin_api_1a7c17a722afa493203f0cd323e_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_262\">RenderCallbacksPluginApi::on_post_shadow</a></span>",
+      "version":"Release 2.1",
+      "name":"struct_render_callbacks_plugin_api_1a7c17a722afa493203f0cd323ebea44a8"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"struct_render_callbacks_plugin_api_1a91a8b53630ca482a80fd99d68_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_259\">RenderCallbacksPluginApi::on_pre_shadow</a></span>",
+      "version":"Release 2.1",
+      "name":"struct_render_callbacks_plugin_api_1a91a8b53630ca482a80fd99d68c673f1a"
+    }],
+  "struct":[],
+  "enum":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"plugin__api_8h_1adaa4ab4336503603b91000ca436f5199_0\" data-reftooltip-refid=\"engine_c\"><a class=\"el\" href=\"plugin__api_8h.html#line_227\">RenderCallbacksPluginApi_EventType</a></span>",
+      "version":"Release 2.1",
+      "name":"plugin__api_8h_1adaa4ab4336503603b91000ca436f5199"
+    }]
+};
