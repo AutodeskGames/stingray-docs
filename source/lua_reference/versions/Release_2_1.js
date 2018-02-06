f4ebbbe77a2efe466a7cff98777d824af1c722ae
Robb Surridge <robb.surridge@autodesk.com>
2018-02-06 14:43:56 -0500

    Add version history for Lua

---
+++ source/lua_reference/versions/Release_2_1.js
@@ -0,0 +1,94 @@
+var version_diff_lua_ref_Release_2_1 = {
+  "tab":[],
+  "con":[],
+  "ns":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"ns_stingray_FileSystem\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_FileSystem.html\">stingray.FileSystem</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.FileSystem"
+    }],
+  "sig":[{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_SteamVR_generic__tracker__local__pose\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_SteamVR.html#sig_stingray_SteamVR_generic__tracker__local__pose\">stingray.SteamVR.generic_tracker_local_pose()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.SteamVR.generic_tracker_local_pose"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_SteamVR_controller__role\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_SteamVR.html#sig_stingray_SteamVR_controller__role\">stingray.SteamVR.controller_role()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.SteamVR.controller_role"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_FileSystem_project__path\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_FileSystem.html#sig_stingray_FileSystem_project__path\">stingray.FileSystem.project_path()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.FileSystem.project_path"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_Oculus_camera\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_Oculus.html#sig_stingray_Oculus_camera\">stingray.Oculus.camera()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.Oculus.camera"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_FileSystem_shutdown\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_FileSystem.html#sig_stingray_FileSystem_shutdown\">stingray.FileSystem.shutdown()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.FileSystem.shutdown"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_SteamVR_generic__tracker__world__pose\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_SteamVR.html#sig_stingray_SteamVR_generic__tracker__world__pose\">stingray.SteamVR.generic_tracker_world_pose()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.SteamVR.generic_tracker_world_pose"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_SteamVR_controller__manufacturer__name\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_SteamVR.html#sig_stingray_SteamVR_controller__manufacturer__name\">stingray.SteamVR.controller_manufacturer_name()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.SteamVR.controller_manufacturer_name"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_FileSystem_exists\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_FileSystem.html#sig_stingray_FileSystem_exists\">stingray.FileSystem.exists()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.FileSystem.exists"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_SteamVR_is__hmd__present\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_SteamVR.html#sig_stingray_SteamVR_is__hmd__present\">stingray.SteamVR.is_hmd_present()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.SteamVR.is_hmd_present"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_SteamVR_camera\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_SteamVR.html#sig_stingray_SteamVR_camera\">stingray.SteamVR.camera()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.SteamVR.camera"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_SteamVR_is__generic__tracker__tracked\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_SteamVR.html#sig_stingray_SteamVR_is__generic__tracker__tracked\">stingray.SteamVR.is_generic_tracker_tracked()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.SteamVR.is_generic_tracker_tracked"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_FileSystem_appdata__path\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_FileSystem.html#sig_stingray_FileSystem_appdata__path\">stingray.FileSystem.appdata_path()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.FileSystem.appdata_path"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_FileSystem_remove__file\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_FileSystem.html#sig_stingray_FileSystem_remove__file\">stingray.FileSystem.remove_file()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.FileSystem.remove_file"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_FileSystem_initialize\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_FileSystem.html#sig_stingray_FileSystem_initialize\">stingray.FileSystem.initialize()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.FileSystem.initialize"
+    },{
+      "change":"added",
+      "display_name":"<span class=\"hasTooltip\" data-reftooltip=\"sig_stingray_FileSystem_make__directory\" data-reftooltip-refid=\"lua_ref\"><a class=\"el\" href=\"ns_stingray_FileSystem.html#sig_stingray_FileSystem_make__directory\">stingray.FileSystem.make_directory()</a></span>",
+      "version":"Release 2.1",
+      "name":"stingray.FileSystem.make_directory"
+    },{
+      "change":"removed",
+      "name":"stingray.SteamVR.controller_setup",
+      "version":"Release 2.1",
+      "display_name":"stingray.SteamVR.controller_setup()"
+    }],
+  "mem":[],
+  "obj":[],
+  "enu":[]
+};
